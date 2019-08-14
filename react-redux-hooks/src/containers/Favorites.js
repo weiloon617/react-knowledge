9124907d75e771f334b7ef326aeb3505239445e3
weiloon617 <weiloon617@gmail.com>
2019-08-14 10:10:28 +0800

    added context api

---
+++ react-redux-hooks/src/containers/Favorites.js
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react'
+
+// Component
+import FavoriteItem from '../components/Favorites/FavoriteItem'
+
+// Context Api
+import { ProductsContext } from '../context/products-context'
+
+// Styling
+import './Products.css'
+
+const Favorites = props => {
+  const favoriteProducts = useContext(ProductsContext).products.filter(
+    p => p.isFavorite,
+  )
+
+  let content = <p className="placeholder">Got no favorites yet!</p>
+  if (favoriteProducts.length > 0) {
+    content = (
+      <ul className="products-list">
+        {favoriteProducts.map(prod => (
+          <FavoriteItem
+            key={prod.id}
+            id={prod.id}
+            title={prod.title}
+            description={prod.description}
+          />
+        ))}
+      </ul>
+    )
+  }
+  return content
+}
+
+export default Favorites
